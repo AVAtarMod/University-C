c6779528f40c5271d943f4c7ddb6aeb38246ea83
AVAtarMod <respawner289@gmail.com>
2020-11-25 13:58:40 +0300

    Add task3()

--- LABW9/tasks.cpp
+++ LABW9/tasks.cpp
@@ -21,6 +21,7 @@ int main()
         task2();
         break;
     case 3:
+        task3();
         break;
     case 4:
         break;
@@ -46,6 +47,7 @@ int task1()
         arrayB[i] = *(arrayA + i) * (*(arrayA + i)) + 2 * (*(arrayA + i)) - 1;
     }
     deleteArray(arrayA);
+
     printArray(arrayB, numberElements, "B");
     deleteArray(arrayB);
 
@@ -65,11 +67,52 @@ int task2()
         arrayB[i] = (abs(arrayA[i]) % 3 == 0) ? true : false;
     }
     deleteArray(arrayA);
+
     printArray(arrayB, numberElements, "B");
     deleteArray(arrayB);
 
     return 0;
 }
 
-int task3();
-int task4();
+int task3()
+{
+
+    int *arrayA = generationAndPrintArray("A");
+    short int numberElements = *arrayA;
+
+    int *arrayB = new int[numberElements];
+    *arrayB = numberElements;
+    for (int i = 1; i <= numberElements; i++)
+    {
+        int result = 0;
+        for (int c = 1, *p = (arrayA + c); c <= i; c++, p++)
+        {
+            result += *p;
+        }
+        arrayB[i] = result;
+    }
+    deleteArray(arrayA);
+
+    printArray(arrayB, numberElements, "B");
+    deleteArray(arrayB);
+
+    return 0;
+}
+
+int task4(){
+    int *arrayA = generationAndPrintArray("A");
+    short int numberElements = *arrayA;
+
+    int *arrayB = new int[numberElements];
+    *arrayB = numberElements;
+    for (int i = 1; i <= numberElements; i++)
+    {
+        int result = 0;
+        for (int c = 1, *p = (arrayA + c); c <= i; c++, p++)
+        {
+            result += *p;
+        }
+        arrayB[i] = result;
+    }
+    
+}
