138fb12870cd3645afecb70b495386b01d65ad39
AVAtarMod <respawner289@gmail.com>
2020-11-25 07:12:56 +0300

    random.cpp edited

--- LABW9/random.cpp
+++ LABW9/random.cpp
@@ -12,14 +12,14 @@ int engine()
     // parameters of std::mt19937 (instance of std::mersenne_twister_engine<>)
     // different across executions of application
     std::mt19937::result_type seed = rd() ^ (
-            (std::mt19937::result_type)
-            std::chrono::duration_cast<std::chrono::seconds>(
-                std::chrono::system_clock::now().time_since_epoch()
-                ).count() +
-            (std::mt19937::result_type)
-            std::chrono::duration_cast<std::chrono::microseconds>(
-                std::chrono::high_resolution_clock::now().time_since_epoch()
-                ).count() );
+        (std::mt19937::result_type)
+        std::chrono::duration_cast<std::chrono::seconds>(
+            std::chrono::system_clock::now().time_since_epoch()
+            ).count() +
+        (std::mt19937::result_type)
+        std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::high_resolution_clock::now().time_since_epoch()
+            ).count() );
 
     std::mt19937 gen(seed);
 
