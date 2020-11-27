137bde1bf5b4bc28d6e064ae00d055c823e09b3d
AVAtarMod <respawner289@gmail.com>
2020-11-27 08:00:57 +0300

    add fine working implementation

--- LABW8/8task_7.cpp
+++ LABW8/8task_7.cpp
@@ -1,25 +1,33 @@
 // Лесенкой   называется   набор   кубиков, в котором  каждый  более  верхний  слой содержит кубиков меньше, чем предыдущий. Требуется написать программу, вычисляющую число лесенок, которое можно построить из N кубиков (1 ≤ N≤ 100)
 
-#include <iostream>
+#include <iostream>`
+using namespace std;
 typedef int blocks;
-int computeNumberStairs(blocks n, int layer);
 
-int main(){
-    std::cout << computeNumberStairs(10,1);
-}
+void stairs(int numberLayer, blocks n);
+int d = 0;
 
-int computeNumberStairs(blocks n, int layer)
+int main()
 {
-    if (n <= 0)
-        return 0;
-    int result = 0;
-
-  int count = 0;
-  for (int level = 1; level < layer; ++level) {
-    if ((n - level) < 0) 
-      break;
-    count += computeNumberStairs(level, n - level);
-  }
-
-    return result;
+    blocks n;
+    cout << "Введите количество кубиков";
+    cin >> n;
+    if (n > 0 && n < 101)
+    {
+        stairs(1, n);
+    }
+    cout << "Количество лесенок = " << d - 1;
+    return 0;
+}
+//В результате считает количество возможных 1 слоев с N-1 кубиками
+void stairs(int numberLayer, blocks n)
+{
+    if (n == 0)
+    {
+        d++;
+    }
+    for (int i = numberLayer; i <= n; i++)
+    {
+        stairs(i + 1, n - i);
+    }
 }
