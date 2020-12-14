bfb97d40b2d39364a66d5c77076505401bcd8f7a
AVAtarMod <respawner289@gmail.com>
2020-12-14 19:58:27 +0300

    see prev. commit

--- LABW11/arrays2d.cpp
+++ LABW11/arrays2d.cpp
@@ -1,12 +1,95 @@
 #include <iostream>
 #include <random>
 
-int *getRangeUser()
+#include "arrays2d.h"
+
+namespace array2d
 {
-    int *choice = new int[2];
 
-    std::printf("Введите числа А и В диапазона [А;В] :");
-    std::scanf("%d %d", &choice[0], &choice[1]);
+    int *getRangeUser()
+    {
+        int *choice = new int[2];
 
-    return choice;
-}
+        std::printf("Введите числа А и В диапазона значений псевдо-случайных чисел [А;В] : ");
+        std::scanf("%d %d", &choice[0], &choice[1]);
+        std::cin.ignore(32767,'\n');
+        if (choice[0] > choice[1])
+        {
+            int temp = choice[0];
+            choice[0] = choice[1];
+            choice[1] = temp;
+        }
+
+        return choice;
+    }
+
+    int **init(const int rows, const int collumns)
+    {
+        int **array = new int *[rows];
+        for (int i = 0; i < collumns; i++)
+        {
+            array[i] = new int[collumns];
+        }
+
+        return array;
+    }
+
+    int **fillRandom(int **array, int leftLim, int rightLim, const int rows, const int collumns)
+    {
+        std::mt19937 engine(static_cast<unsigned long>(clock()));
+        std::uniform_int_distribution<int> random(leftLim, rightLim);
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int coll = 0; coll < collumns; coll++)
+            {
+                array[row][coll] = random(engine);
+            }
+        }
+
+        return array;
+    }
+
+    void print(int **array, const int collumns, const int rows, const char *text, int maxElement)
+    {
+        maxElement = numDigits(maxElement);
+        printf("\nМассив %d x %d %s :\n", collumns, rows, text);
+
+        if (maxElement == 0)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                printf("%d | ", row);
+                for (int coll = 0; coll < collumns; coll++)
+                {
+                    printf("%d,", array[row][coll]);
+                }
+                printf("\b |\n");
+            }
+        }
+        else
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                printf("%d | ", row);
+                for (int coll = 0; coll < collumns; coll++)
+                {
+                    printf("%*d,", (maxElement + 1), array[row][coll]);
+                }
+                printf("\b |\n");
+            }
+        }
+    }
+
+    int numDigits(int number)
+    {
+        number = abs(number);
+        int result = 0;
+        for (int digit = 1; number % digit != number; digit *= 10)
+        {
+            result++;
+        }
+        return result;
+    }
+
+} // namespace array2d
