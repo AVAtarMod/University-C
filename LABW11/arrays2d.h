d0ce8bf4b3ea10ba407baae3f44dd3990c39cd72
AVAtarMod <respawner289@gmail.com>
2020-12-14 19:57:29 +0300

    add namespace array2d, 4 functions, stdin protect
    stdin "protect" is cin.ignore which clean trash
    (excess numbers and chars)

--- LABW11/arrays2d.h
+++ LABW11/arrays2d.h
@@ -1,6 +1,18 @@
 #ifndef ARRAYS_2D
 #define ARRAYS_2D
 
-int *getRangeUser();
+namespace array2d
+{
+
+    int *getRangeUser();
+
+    int **init(const int rows, const int collumns);
+    int **fillRandom(int **array, int leftLim, int rightLim, const int rows, const int collumns);
+
+    void print(int **array, const int collumns, const int rows, const char *text = "", int maxElement = 0);
+
+    int numDigits(int number);
+
+} // namespace array2d
 
 #endif
