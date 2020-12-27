729781ab7c9b0556bacb668cc4926135126d6e40
AVAtarMod <respawner289@gmail.com>
2020-12-27 21:50:31 +0300

    add 1,2,3 tasks implementation

--- LABW12/main.cpp
+++ LABW12/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 #include "string.h"
+#include "useful.h"
+
+using std::cin;
+using std::cout;
+
+void task1cstr();
+void task2cstr();
+void task3cstr();
+void task4cstr();
+void task7cstr();
 
 void task1();
 void task2();
@@ -8,12 +18,15 @@ void task3();
 void task4();
 void task7();
 
+int choiceImplementation();
+
 int main(int argc, char const *argv[])
 {
     bool noData = false;
     if (argc == 1)
     {
-        printf("###Вариант 3 ###\nВведите программу для запуска: ");
+        printf("### Вариант 3 ###");
+        printf("\nВведите программу для запуска: ");
         argc++;
         noData = true;
     }
@@ -23,7 +36,6 @@ int main(int argc, char const *argv[])
         if (noData)
         {
             scanf("%d", &choice);
-            std::cin.ignore(32767, '\n');
         }
         else
         {
@@ -34,23 +46,38 @@ int main(int argc, char const *argv[])
         {
         case 1:
             printf("Задание №1\n");
-            task1();
+            if (choiceImplementation() == 1)
+                task1cstr();
+            else
+                task1();
             break;
         case 2:
             printf("Задание №2\n");
-            task2();
+            if (choiceImplementation() == 1)
+                task2cstr();
+            else
+                task2();
             break;
         case 3:
             printf("Задание №3\n");
-            task3();
+            if (choiceImplementation() == 1)
+                task3cstr();
+            else
+                task3();
             break;
         case 4:
             printf("Задание №4\n");
-            task4();
+            if (choiceImplementation() == 1)
+                task4cstr();
+            else
+                task4();
             break;
         case 7:
             printf("Задание №7\n");
-            task7();
+            if (choiceImplementation() == 1)
+                task7cstr();
+            else
+                task7();
             break;
 
         default:
@@ -60,19 +87,82 @@ int main(int argc, char const *argv[])
     }
 }
 
+int choiceImplementation()
+{
+    while (true)
+    {
+        int choice = printAndScan<int>("Выберите реализацию: c-string[1] или std::string[2]: ");
+        if (choice == 1 || choice == 2)
+        {
+            std::cin.ignore(32767, '\n');
+            return choice;
+        }
+        else
+            cout << "Введеные данные неверны.\n";
+    }
+}
+
+void task1cstr()
+{
+    char *line = cstr::getStringUser(true);
+    char symbol = cstr::getSymbolUser(" для замены на другой символ");
+    int numberReplaces;
+    int *indexes = cstr::indexSymbols(line, symbol, numberReplaces);
+
+    char replace = cstr::getSymbolUser(", на который нужно заменить");
+    cstr::replace(line, indexes, numberReplaces, replace);
+    cstr::print(line);
+
+    delete[] line;
+    delete[] indexes;
+}
 void task1()
 {
-    char *line = cstr::getString(true);
-    
-    delete[] line;
+    std::string str = classStr::getString(true);
+    uint8_t replaces;
+    uint8_t *indexes = classStr::indexSymbols(str, cstr::getSymbolUser(" для замены на другой символ"), replaces);
+    classStr::replace(str, indexes, replaces, cstr::getSymbolUser(", на который нужно заменить"));
+
+    classStr::print(str);
+    delete[] indexes;
+}
+void task2cstr()
+{
+    char *telegram = cstr::getStringUser(true);
+    uint16_t words = cstr::countWords(telegram);
+    uint16_t cost = printAndScan<int>("Введите стоимость телеграммы: ");
+    cout << "Стоимость телеграммы: " << words * cost << "\n";
+
+    delete[] telegram;
 }
 void task2()
 {
+    std::string str = classStr::getString(true);
+    uint16_t words = classStr::countWords(str);
+    uint16_t cost = printAndScan<int>("Введите стоимость телеграммы: ");
+    cout << "Стоимость телеграммы: " << words * cost << "\n";
+}
+void task3cstr()
+{
+    char *line = cstr::getStringUser(true);
+    cstr::changeCase(line);
+    cstr::print(line);
+
+    delete[] line;
 }
 void task3()
 {
+    std::string str = classStr::getString(true);
+    classStr::changeCase(str);
+    classStr::print(str);
+}
+void task4cstr()
+{
 }
 void task4()
+{
+}
+void task7cstr()
 {
 }
 void task7()
