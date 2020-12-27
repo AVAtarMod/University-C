c1a4b5b72365186d2f5cf8fa62bb7340114c3a0d
AVAtarMod <respawner289@gmail.com>
2020-12-27 21:49:18 +0300

    add funtions

--- LABW12/string.h
+++ LABW12/string.h
@@ -3,14 +3,34 @@
 
 namespace cstr
 {
-    char *getString(bool isLine);
-    
-    int *indexSymbols(char *string, char symbol);
+    char getSymbolUser(const char *text = " ");
+    char *getStringUser(bool isLine);
+
+    int countWords(char *string);
+    int *indexSymbols(char *string, char symbol, int &count);
+
+    void changeCase(char *string);
+    void replace(char *string, int *indexes, int lengthIndexes, char symbol);
+    void print(const char *string);
+    void doOffset(char *string, int offset);
 } // namespace cstr
 
-namespace string
+namespace classStr
 {
+    using std::string;
+    //////////////////////////////
 
-} // namespace string
+    string getString(bool isLine);
+
+    uint16_t countWords(string str);
+
+    uint8_t *indexSymbols(string str, char symbol, uint8_t &numberMatches);
+
+    void changeCase(string &str);
+    void replace(string &str, uint8_t *indexes, uint8_t numberReplaces, char replace);
+    void print(string str);
+} // namespace classStr
+
+int choiceCase();
 
 #endif
