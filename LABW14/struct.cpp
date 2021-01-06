a96c229ec077b6c1d5fdee0d212052d5eee0e6a3
AVAtarMod <respawner289@gmail.com>
2021-01-06 07:12:05 +0300

    new buffer-based mechanics

--- LABW14/struct.cpp
+++ LABW14/struct.cpp
@@ -1,34 +1,64 @@
 #include <iostream>
+#include <fstream>
 #include <locale>
 
 #include "struct.h"
 
+bool binWriteMember(std::ofstream &output, Member &member)
+{
+    std::unique_ptr<char[]> memberBinary = member;
+    output.write(memberBinary.get(), BUFFER_SIZE);
+    if (output.good())
+        return true;
+    else
+        return false;
+}
+
+bool binReadMember(std::ifstream &input, Member &member)
+{
+    char buffer[BUFFER_SIZE];
+    input.read(buffer, BUFFER_SIZE);
+    member = static_cast<Member>(buffer);
+    if (input.good())
+        return true;
+    else
+        return false;
+}
+
 std::istream &operator>>(std::istream &input, Member &member)
 {
-    setlocale(LC_ALL,"");
+    setlocale(LC_ALL, "ru_RU.UTF8");
+
+    input.ignore(INT16_MAX, '\n');
     std::cout << "Enter\n- fullname: ";
-    //TODO Add getline impl
-    // input.getline(member.fullName);
-    std::cout << "\n- country: ";
-    input >> member.country;
-    std::cout << "\n- city: ";
-    input >> member.city;
-    std::cout << "\n- work (place): ";
-    input >> member.work;
-    std::cout << "\n- age: ";
+    input.getline(member.fullName, STR_SIZE, '\n');
+    input.ignore(INT16_MAX, '\n');
+
+    std::cout << "- country: ";
+    input.getline(member.country, STR_SIZE, '\n');
+    input.ignore(INT16_MAX, '\n');
+
+    std::cout << "- city: ";
+    input.getline(member.city, STR_SIZE, '\n');
+    input.ignore(INT16_MAX, '\n');
+
+    std::cout << "- work (place): ";
+    input.getline(member.work, STR_SIZE, '\n');
+    input.ignore(INT16_MAX, '\n');
+
+    std::cout << "- age: ";
     input >> member.age;
-    std::cout << "\n- phone number: ";
+    std::cout << "- phone number: ";
     input >> member.phoneNumber;
+    
     return input;
 }
 
 std::ostream &operator<<(std::ostream &output, Member &member)
 {
-    setlocale(LC_ALL,"");
+    setlocale(LC_ALL, "");
     std::cout << member.fullName << " " << member.age << "y.o"
               << "\nCountry, city: " << member.country << "," << member.city << "\nWork place: " << member.work
-              << "\nPhone number: " << member.phoneNumber;
+              << "\nPhone number: " << member.phoneNumber << "\n";
     return output;
 }
-
-
--- LABW14/struct.h
+++ LABW14/struct.h
@@ -1,22 +1,90 @@
 #ifndef STRUCT
 #define STRUCT
 
+#include <memory>
+#include <cstring>
+#define STR_SIZE 255
+#define BUFFER_SIZE 1032 /** @def BUFFER_SIZE 1032 @brief 1032 bytes*/
+
+template <class T>
+std::unique_ptr<char[]> numberBinary(T number)
+{
+    //TODO Строка результата больше предпологаемого размера;
+    auto bin = std::make_unique<char[]>(sizeof(number));
+    strncpy(bin.get(), std::to_string(number).c_str(), sizeof(number));
+    return bin;
+}
+
+template <class T>
+T numberDecimal(char* numberBinary)
+{
+    return static_cast<T>(atoll(numberBinary));
+}
+
 struct Member
 {
-    std::string fullName = "";
-    std::string country = "";
-    std::string city = "";
-    std::string work = "";
-    uint32_t age = 0;
-    uint64_t phoneNumber = 0;
-    explicit operator const char *()
+    char fullName[STR_SIZE];
+    char country[STR_SIZE];
+    char city[STR_SIZE];
+    char work[STR_SIZE];
+    uint32_t age;
+    uint64_t phoneNumber;
+
+    /**
+     * @brief Convert structure to char* buffer BUFFER_SIZE bytes
+     * @return smart pointer to char*
+     */
+    operator std::unique_ptr<char[]>()
     {
-        std::string result = fullName + country + city + work + std::to_string(age) + std::to_string(phoneNumber);
-        return result.c_str();
+        auto ageBuf = numberBinary(age);
+        auto phoneNumberBuf = numberBinary(phoneNumber);
+
+        auto buffer = std::make_unique<char[]>(BUFFER_SIZE);
+        char *ptrCopy = buffer.get();
+
+        /**
+         * @brief Filling main buffer from Member
+         */
+        memcpy(ptrCopy            , fullName, STR_SIZE);
+        memcpy(ptrCopy += STR_SIZE, country, STR_SIZE);
+        memcpy(ptrCopy += STR_SIZE, city, STR_SIZE);
+        memcpy(ptrCopy += STR_SIZE, work, STR_SIZE);
+        memcpy(ptrCopy += STR_SIZE, (char *)(&age), sizeof(age));
+        memcpy(ptrCopy += sizeof(age),(char *)(&phoneNumber),
+        sizeof(phoneNumber));
+
+        return buffer;
+    }
+
+    /**
+     * @brief Default constructor
+     */
+    Member(){}
+
+    /**
+     * @brief Construct from buffer
+     * @param buffer[1032] extracted from binary file or other place;
+     */
+    Member(char buffer[BUFFER_SIZE])
+    {
+        char *ptrBuffer = buffer;
+        char ageBuff[sizeof(age)];
+        char numberPhoneBuff[sizeof(phoneNumber)];
+        strncpy(fullName,       ptrBuffer, STR_SIZE);
+        strncpy(country,        ptrBuffer+=STR_SIZE, STR_SIZE);
+        strncpy(city,           ptrBuffer+=STR_SIZE, STR_SIZE);
+        strncpy(work,           ptrBuffer+=STR_SIZE, STR_SIZE);
+        strncpy(ageBuff,        ptrBuffer+=STR_SIZE, sizeof(age));
+        strncpy(numberPhoneBuff,ptrBuffer+=sizeof(age), sizeof(phoneNumber));
+        age = *((uint32_t *)(ageBuff));
+        phoneNumber = *((uint64_t *)(numberPhoneBuff));
     }
 };
 
 std::istream &operator>>(std::istream &input, Member &member);
 std::ostream &operator<<(std::ostream &output, Member &member);
+
+bool binWriteMember(std::ofstream &output, Member &member);
+bool binReadMember(std::ifstream &input, Member &member);
 
 #endif
