6a37cc8c99bfd4deed27e10b64918d8376267801
AVAtarMod <respawner289@gmail.com>
2021-01-06 13:22:25 +0300

    add functions, refactor

--- LABW14/vector.cpp
+++ LABW14/vector.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 
 #include "vector.h"
 
@@ -25,19 +26,71 @@ std::vector<Member> userInput()
     return members;
 }
 
-void print(std::vector<Member> members)
+void print(std::vector<Member> members, const char* text = "")
 {
+    std::cout << "========= Members" << text << " =========\n";
     for (uint16_t i = 0; i < members.size(); i++)
     {
         std::cout << "Member #" << i + 1 << "\n";
-        std::cout << members.at(i);
+        std::cout << members.at(i) << "\n";
     }
+    std::cout << "=========         =========" << std::endl;
 }
 
 void binPrint(std::string file, std::vector<Member> storage)
 {
     binInput(file, storage, true);
+    print(storage," from file");
+}
+
+void deleteMember(std::vector<Member> &storage)
+{
+    uint16_t numberMember;
+    std::cout << "Choice number member for delete it (";
+    for (uint16_t i = 0; i < storage.size(); i++)
+    {
+        std::cout << i << "; ";
+    }
+    std::cout << "\b\b)\nEnter: ";
+    std::cin >> numberMember;
+    std::cin.ignore(INT16_MAX, '\n');
+    if (numberMember < storage.size())
+        storage.erase(storage.begin() + numberMember);
+
     print(storage);
+    std::string file;
+    std::cout << "Enter name file[.txt] for output current members: ";
+    std::cin >> file;
+    txtOutput(file, storage);
+}
+
+void sort(std::vector<Member> &storage)
+{
+    uint32_t act;
+    uint32_t sort;
+    std::cout << "Avaliable actions:\n\t"
+                 "1) Sort for age\n\t"
+                 "[1]lesser,[2]bigger\n\t"
+                 "Enter (ex 1 2 for bigger age): ";
+    std::cin >> act >> sort;
+    switch (act)
+    {
+    case 1:
+        if (sort == 1)
+            std::sort(storage.begin(), storage.end(), leastAge());
+        else if (sort == 2)
+            std::sort(storage.begin(), storage.end(), biggerAge());
+        break;
+
+    default:
+        break;
+    }
+
+    print(storage);
+    std::string file;
+    std::cout << "Enter name file[.txt] for output current members: ";
+    std::cin >> file;
+    txtOutput(file, storage);
 }
 
 void binInput(std::string file, std::vector<Member> &storage, bool readUntilEof)
@@ -55,28 +108,18 @@ void binInput(std::string file, std::vector<Member> &storage, bool readUntilEof)
             if (storage.size() != sizeNumeric)
                 storage.resize(sizeNumeric);
             for (size_t i = 0; i < sizeNumeric && input.good(); i++)
-            {
                 if (!binReadMember(input, storage.at(i)))
-                {
                     std::cerr << "Error at write Member #" << i + 1 << "!\n";
-                }
-            }
         }
         else
-        {
             for (size_t i = 0; i < storage.size() && input.good(); i++)
-            {
                 if (!binReadMember(input, storage.at(i)))
-                {
                     std::cerr << "Error at read Member #" << i + 1 << "!\n";
-                }
-            }
-        }
     }
 
     else
     {
-        std::cerr << "File not exist or corrupt\n";
+        std::cerr << "File" << file << " not exist or corrupt\n";
     }
 
     input.close();
@@ -101,7 +144,21 @@ void binOutput(std::string file, std::vector<Member> storage)
         }
     }
     else
-        std::cerr << "File not exist or corrupt\n";
+        std::cerr << "File " << file << " not exist or corrupt\n";
 
     output.close();
+}
+
+void txtOutput(std::string file, std::vector<Member> str)
+{
+    std::ofstream txtOut(file);
+    if (txtOut.good())
+    {
+        for (size_t i = 0; i < str.size(); i++)
+            txtOut << str.at(i);
+    }
+    else
+        std::cerr << "File " << file << " not exist or corrupt\n";
+
+    txtOut.close();
 }
--- LABW14/vector.h
+++ LABW14/vector.h
@@ -6,9 +6,21 @@
 #include "struct.h"
 
 std::vector<Member> userInput();
-void binInput(std::string file, std::vector<Member> &storage, bool readUntilEof);
 
-void print(std::vector<Member>);
+void print(std::vector<Member> members, const char *text = "");
+void deleteMember(std::vector<Member> &storage);
+void sort(std::vector<Member> &storage);
+
+void binPrint(std::string file, std::vector<Member> storage);
+void binInput(std::string file, std::vector<Member> &storage, bool readUntilEof);
 void binOutput(std::string file, std::vector<Member> storage);
 
+void txtOutput(std::string file, std::vector<Member> str);
+
+bool isEmpty(std::vector<Member> str)
+{
+    bool result = str.empty();
+    if (result) std::cerr << "Array members is empty. NOTE: act #9 not save data to array.\n";
+    return result;
+}
 #endif
