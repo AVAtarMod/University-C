116b0c5cdc3da377cd3e9acc6b6bdec91e821609
AVAtar Mod <respawner289@gmail.com>
2021-10-07 18:53:40 +0300

    rm: printAndScan from functions lib

--- 3SST/Libraries/functions.hpp
+++ 3SST/Libraries/functions.hpp
@@ -9,14 +9,4 @@ int getRandomNumber(int from, int to);
 void replace(char *string, int *indexes, int lengthIndexes, char symbol);
 int *indexSymbols(char *string, char symbol, int &count);
 
-template<class T>
-T printAndScan(const char* text = ""){
-    T data;
-    std::cout << text;
-    std::cin >> data;
-    std::cin.ignore(32767, '\n');
-
-    return data;
-}
-
 #endif //FUNCTIONS_LIB
