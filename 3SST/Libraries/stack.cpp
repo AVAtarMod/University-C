7cf590f2a31c2733df7ccd62cc0ebc51e212af0c
AVAtar Mod <respawner289@gmail.com>
2021-10-07 18:57:32 +0300

    rf: type rename. new: templates

--- 3SST/Libraries/stack.cpp
+++ 3SST/Libraries/stack.cpp
@@ -1,16 +1,1 @@
 #include "stack.hpp"
-
-bool empty(Stack stack_data)
-{
-    return (stack_data != nullptr && *stack_data != nullptr);
-}
-
-Stack_element *pop(Stack &stack_data)
-{
-    return popFront(stack_data);
-}
-
-void push(Stack &stack, int data)
-{
-    pushFront(stack, data);
-}
--- 3SST/Libraries/stack.hpp
+++ 3SST/Libraries/stack.hpp
@@ -3,7 +3,33 @@
 
 #include "list.hpp"
 
-typedef IntList1D Stack;
-typedef IntList1D_element Stack_element;
-    
+typedef IntList1D IntStack;
+typedef IntList1D_element IntStack_element;
+
+typedef IntList1D_element IntStack_element;
+
+template <class T>
+using Stack_element = List1D_element<T>;
+
+template <class T>
+using Stack = List1D<T>;
+
+template <class T>
+bool isEmpty(Stack<T> stack_data)
+{
+    return (stack_data == nullptr || *stack_data == nullptr);
+}
+
+template <class T>
+Stack_element<T> *pop(Stack<T> &stack)
+{
+    return popFront(stack);
+}
+
+template <class T>
+void push(Stack<T> &stack, T data)
+{
+    pushFront(stack, data);
+}
+
 #endif //STACK_LIB
