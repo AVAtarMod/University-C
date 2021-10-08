0fe398d0a7dcb327c121da4ffe483e020a410c93
AVAtar Mod <respawner289@gmail.com>
2021-10-08 06:59:06 +0300

    fix: braces check Logic of check was replaced to the sample from lecture

--- 3SST/3LW/LW_lib.cpp
+++ 3SST/3LW/LW_lib.cpp
@@ -1,44 +1,38 @@
 #include "LW_lib.hpp"
 
-IntList1D checkBraces(const std::string braces, const std::string strWithBraces)
+bool checkBraces(const std::string braces, const std::string strWithBraces)
 {
-    const int DEFAULT_VALUE = 0;
+    Stack<uint> openBraces = nullptr;
+    bool strIsValid = true;
 
-    Stack<int> open_braces = nullptr;
-    Stack<int> closed_braces = nullptr;
-    IntList1D invalidIdx = nullptr;
+    const uint bracesLen = braces.size();
     const uint strLen = strWithBraces.size();
 
-    for (uint16_t i = 0; i < strLen; ++i)
+    for (uint16_t i = 0; i < strLen && strIsValid; ++i)
     {
         char current = strWithBraces[i];
-        const uint64_t bracesIdx = braces.find_first_of(current);
-        if (bracesIdx != braces.npos)
+        const uint braceIdx = braces.find_first_of(current);
+        if (braceIdx <= bracesLen)
         {
-            if (bracesIdx % 2 == 0)
-                push(open_braces, static_cast<int>(i));
+            if (braceIdx % 2 == 0)
+                push(openBraces, static_cast<uint>(braceIdx));
             else
-                push(closed_braces, static_cast<int>(i));
+            {
+                Stack_element<uint> *lastBrace = pop(openBraces);
+                bool isError = (lastBrace == nullptr ||
+                                lastBrace->data + 1 != braceIdx);
+                if (isError)
+                    strIsValid = false;
+                delete lastBrace;
+            }
         }
     }
-    while (!isEmpty(open_braces) || !isEmpty(closed_braces))
-    {
-        Stack_element<int> *curOpen = pop(open_braces);
-        Stack_element<int> *curClosed = pop(closed_braces);
-        bool isError =
-            curOpen == nullptr ||
-            curClosed == nullptr ||
-            (curOpen->data > curClosed->data);
-        if (isError)
-            pushBack(invalidIdx, DEFAULT_VALUE);
 
-        if (curOpen != nullptr)
-            delete curOpen;
-        if (curClosed != nullptr)
-            delete curClosed;
+    if (!isEmpty(openBraces))
+    {
+        deleteStack(openBraces);
+        strIsValid = false;
     }
 
-    if (invalidIdx != nullptr)
-        appendBack(invalidIdx, mergeUpperSorted(open_braces, closed_braces, true));
-    return invalidIdx;
+    return strIsValid;
 }
