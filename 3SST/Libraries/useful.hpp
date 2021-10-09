2c478b90e58dcb7d6b7450600630b000eb2b2d65
AVAtar Mod <respawner289@gmail.com>
2021-10-09 08:32:39 +0300

    new: useful.cpp: move definitions to .cpp file

--- 3SST/Libraries/useful.hpp
+++ 3SST/Libraries/useful.hpp
@@ -2,6 +2,7 @@
 #define USEFUL
 
 #include <iostream>
+#include <string>
 
 template <class T>
 T printAndScan(const char *text = "")
@@ -13,61 +14,56 @@ T printAndScan(const char *text = "")
     return data;
 }
 
-void cinReset()
-{
-    std::cin.ignore();
-    if (std::cin.eof())
-        std::cin.clear();
-}
+void cinReset();
 
-std::string reset = "\033[0m";
+extern std::string reset;
 
 // Black
-std::string fgBlack = "\033[30m";
-std::string fgBrightBlack = "\033[30;1m";
-std::string bgBlack = "\033[40m";
-std::string bgBrightBlack = "\033[40;1m";
+extern std::string fgBlack;
+extern std::string fgBrightBlack;
+extern std::string bgBlack;
+extern std::string bgBrightBlack;
 
 // Red
-std::string fgRed = "\033[31m";
-std::string fgBrightRed = "\033[31;1m";
-std::string bgRed = "\033[41m";
-std::string bgBrightRed = "\033[41;1m";
+extern std::string fgRed;
+extern std::string fgBrightRed;
+extern std::string bgRed;
+extern std::string bgBrightRed;
 
 // Green
-std::string fgGreen = "\033[32m";
-std::string fgBrightGreen = "\033[32;1m";
-std::string bgGreen = "\033[42m";
-std::string bgBrightGreen = "\033[42;1m";
+extern std::string fgGreen;
+extern std::string fgBrightGreen;
+extern std::string bgGreen;
+extern std::string bgBrightGreen;
 
 // Yellow
-std::string fgYellow = "\033[33m";
-std::string fgBrightYellow = "\033[33;1m";
-std::string bgYellow = "\033[43m";
-std::string bgBrightYellow = "\033[43;1m";
+extern std::string fgYellow;
+extern std::string fgBrightYellow;
+extern std::string bgYellow;
+extern std::string bgBrightYellow;
 
 // Blue
-std::string fgBlue = "\033[34m";
-std::string fgBrightBlue = "\033[34;1m";
-std::string bgBlue = "\033[44m";
-std::string bgBrightBlue = "\033[44;1m";
+extern std::string fgBlue;
+extern std::string fgBrightBlue;
+extern std::string bgBlue;
+extern std::string bgBrightBlue;
 
 // Magenta
-std::string fgMagenta = "\033[35m";
-std::string fgBrightMagenta = "\033[35;1m";
-std::string bgMagenta = "\033[45m";
-std::string bgBrightMagenta = "\033[45;1m";
+extern std::string fgMagenta;
+extern std::string fgBrightMagenta;
+extern std::string bgMagenta;
+extern std::string bgBrightMagenta;
 
 // Cyan
-std::string fgCyan = "\033[36m";
-std::string fgBrightCyan = "\033[36;1m";
-std::string bgCyan = "\033[46m";
-std::string bgBrightCyan = "\033[46;1m";
+extern std::string fgCyan;
+extern std::string fgBrightCyan;
+extern std::string bgCyan;
+extern std::string bgBrightCyan;
 
 // White
-std::string fgWhite = "\033[37m";
-std::string fgBrightWhite = "\033[37;1m";
-std::string bgWhite = "\033[47m";
-std::string bgBrightWhite = "\033[47;1m";
+extern std::string fgWhite;
+extern std::string fgBrightWhite;
+extern std::string bgWhite;
+extern std::string bgBrightWhite;
 
 #endif
