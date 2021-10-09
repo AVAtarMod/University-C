817b1183ed8205fe1552f7a778086a4fc24cff8c
AVAtar Mod <respawner289@gmail.com>
2021-10-09 08:39:44 +0300

    new: power(a,b) template

--- 3SST/Libraries/functions.hpp
+++ 3SST/Libraries/functions.hpp
@@ -6,7 +6,17 @@
 int getRandomNumber(int from, int to);
 
 // String functions
+// TODO move to string library
 void replace(char *string, int *indexes, int lengthIndexes, char symbol);
 int *indexSymbols(char *string, char symbol, int &count);
+
+template <class T>
+T pow(T a, int power)
+{
+    if (power == 1)
+        return a;
+    else
+        return pow(a * a, power - 1);
+}
 
 #endif //FUNCTIONS_LIB
