4b93329a6f78112c386241a0f7b7c770e7ec524e
AVAtar Mod <respawner289@gmail.com>
2021-10-25 11:11:45 +0300

    longNumbers: rf: codestyle. new: class and methods

---
+++ 3SST/Libraries/longNumbers.cpp
@@ -0,0 +1,108 @@
+#include "longNumbers.hpp"
+
+UnsignedLongNumber LongNumber::StrToList(const char* str) {
+    bool isPositive = true;
+    const int partSize = getNumberDigits(storageBase_ - 1);
+    UnsignedLongNumber result = nullptr;
+
+    for (uint i = 0; str[i] != '\0'; ++i) {
+        if (isPositive && str[i] == '-')
+            isPositive = positive_ = false;
+        else if (isdigit(str[i])) {
+            uint8_t number = conversion::charToInt(str[i]);
+            pushFront(result, number);
+        }
+    }
+    return result;
+}
+
+LongNumber::LongNumber(UnsignedLongNumber numberList)
+    : list_(numberList), first_(nullptr), last_(nullptr) {
+    update();
+}
+LongNumber::LongNumber(const char* cstring) : list_(StrToList(cstring)) {
+    update();
+}
+
+LongNumber::LongNumber(int number) {
+    while (number != 0) {
+        pushFront(list_, static_cast<uint8_t>(number % storageBase_));
+    }
+}
+
+void LongNumber::print() {
+    if (initialized_) {
+        UnsignedLongNumber_element* tmp = last_;
+        while (tmp->previous != nullptr) {
+            std::cout << static_cast<int>(tmp->data) << " ";
+            tmp = tmp->previous;
+        }
+        std::cout << static_cast<int>(tmp->data);
+    }
+}
+
+void LongNumber::update() {
+    bool isInitialized = isInited(list_);
+    uint size = 0;
+
+    initialized_ = isInitialized;
+    first_ = (isInitialized) ? *list_ : nullptr;
+
+    UnsignedLongNumber_element* tmp = first_;
+
+    if (tmp != nullptr) {
+        while (tmp->next != nullptr) {
+            tmp = tmp->next;
+            ++size;
+        }
+
+        last_ = tmp;
+    } else {
+        last_ = first_;
+    }
+    size_ = size + 1;
+}
+
+LongNumber LongNumber::operator+(const LongNumber& b) const {
+    const LongNumber& a = *this;
+    UnsignedLongNumber result = nullptr;
+
+    UnsignedLongNumber_element* a_ptr = (a.initialized_) ? *a.list_ : nullptr;
+    UnsignedLongNumber_element* b_ptr = (b.initialized_) ? *b.list_ : nullptr;
+
+    uint8_t remainder = 0;
+    while (a_ptr || b_ptr) {
+        uint currentDataA = (a_ptr) ? a_ptr->data : 0;
+        uint currentDataB = (b_ptr) ? b_ptr->data : 0;
+
+        const uint sum = currentDataA + currentDataB + remainder;
+        const uint8_t digit = sum % 10;
+        remainder = (sum - digit) / 10;
+
+        pushBack(result, digit);
+
+        a_ptr = (a_ptr && a_ptr->next) ? a_ptr->next : nullptr;
+        b_ptr = (b_ptr && b_ptr->next) ? b_ptr->next : nullptr;
+    }
+    if (remainder != 0)
+        pushBack(result, remainder);
+
+    return LongNumber(result);
+}
+
+LongNumber LongNumber::operator*(const LongNumber& b) const {
+    const LongNumber& a = *this;
+    UnsignedLongNumber result = nullptr;
+    return LongNumber(result);
+}
+
+std::istream& operator>>(std::istream& input, LongNumber& number) {
+    char* buffer = new char[number.bufferSize_];
+    input.getline(buffer, number.bufferSize_);
+
+    number.list_ = number.StrToList(buffer);
+
+    number.update();
+    delete[] buffer;
+    return input;
+}
--- 3SST/Libraries/longNumbers.hpp
+++ 3SST/Libraries/longNumbers.hpp
@@ -1,6 +1,53 @@
 #ifndef LONGNUMBERS_LIB
 #define LONGNUMBERS_LIB
 
+#include <iostream>
+
 #include "list.hpp"
-    
-#endif //LONGNUMBERS_LIB
+#include "string.hpp"
+
+template <class T> using LongNumber_element = List2D_element<T>;
+
+using UnsignedLongNumber = List2D<uint8_t>;
+using UnsignedLongNumber_element = List2D_element<uint8_t>;
+
+/**
+ * TODO move all libs to include path
+ * TODO add signed number support
+ *
+ * @brief Long Number class
+ */
+class LongNumber {
+  private:
+    uint bufferSize_ = UINT8_MAX;
+    uint storageBase_ = 10;
+    uint size_ = 0;
+
+    UnsignedLongNumber list_;
+    UnsignedLongNumber_element* first_;
+    UnsignedLongNumber_element* last_;
+
+    bool positive_ = true;
+    bool initialized_ = false;
+
+    UnsignedLongNumber StrToList(const char* str);
+
+  public:
+    LongNumber(UnsignedLongNumber numberList = nullptr);
+    LongNumber(const char* cstring);
+    LongNumber(int number);
+
+    const uint getSize() const { return size_; }
+    const uint isInitialized() const { return initialized_; }
+
+    void print();
+    void update();
+
+    LongNumber operator+(const LongNumber& b) const;
+    LongNumber operator*(const LongNumber& b) const;
+    friend std::istream& operator>>(std::istream& input, LongNumber& number);
+
+    ~LongNumber() { deleteList(list_); }
+};
+
+#endif // LONGNUMBERS_LIB
