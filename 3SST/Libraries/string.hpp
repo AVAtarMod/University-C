208fafd99ac9e83b4fbe963696fb29f84568b7a9
AVAtar Mod <respawner289@gmail.com>
2021-10-25 11:19:05 +0300

    string: rf: codestyle. new: charToInt

--- 3SST/Libraries/string.hpp
+++ 3SST/Libraries/string.hpp
@@ -3,28 +3,26 @@
 
 #include <string>
 
-namespace stringLib
-{
-    std::string erase(char c, const std::string string);
-} // namespace string
+namespace stringLib {
+std::string erase(char c, const std::string string);
+} // namespace stringLib
 
-namespace conversion
-{
-    template <class T>
-    T strToType(std::string str)
-    {
-        if (std::is_same_v<T, float>)
-            return std::stof(str);
-        else if (std::is_same_v<T, int>)
-            return std::stoi(str);
-        else if (std::is_same_v<T, long>)
-            return std::stol(str);
-        else if (std::is_same_v<T, uint>)
-            return static_cast<uint>(std::stol(str));
-        else
-            return 0;
-    }
+namespace conversion {
+template <class T> T strToType(std::string str) {
+    if (std::is_same_v<T, float>)
+        return std::stof(str);
+    else if (std::is_same_v<T, int>)
+        return std::stoi(str);
+    else if (std::is_same_v<T, long>)
+        return std::stol(str);
+    else if (std::is_same_v<T, uint>)
+        return static_cast<uint>(std::stol(str));
+    else
+        return 0;
+}
+
+uint8_t charToInt(char digit) { return digit - '0'; }
 
 } // namespace conversion
 
-#endif //STRING_LIB
+#endif // STRING_LIB
