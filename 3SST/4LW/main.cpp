a89665926b5054dd64050de1d7e578e8365c4b68
AVAtar Mod <respawner289@gmail.com>
2021-11-30 13:47:45 +0300

    LW4: add task4, rf: catch exception at main

--- 3SST/4LW/main.cpp
+++ 3SST/4LW/main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <iostream>
 
+// TODO: fix include
 #include "longNumbers.hpp"
 
 void task1();
@@ -25,31 +26,36 @@ int main(int argc, char const* argv[]) {
             choice = std::stoi(argv[i]);
         }
 
-        switch (choice) {
-        case 1:
-            std::cout << "Задание №1\n";
-            task1();
-            std::cout << "-----\n";
-            break;
-        case 2:
-            std::cout << "Задание №2\n";
-            task2();
-            std::cout << "-----\n";
-            break;
-        case 3:
-            std::cout << "Задание №3\n";
-            task3();
-            std::cout << "-----\n";
-            break;
-        case 4:
-            std::cout << "Задание №4\n";
-            task4();
-            std::cout << "-----\n";
-            break;
-        default:
-            std::cout << "Номер задачи введен не верно либо не удалось "
-                         "конвертировать введенные данные\n";
-            break;
+        try {
+            switch (choice) {
+            case 1:
+                std::cout << "Задание №1\n";
+                task1();
+                std::cout << "-----\n";
+                break;
+            case 2:
+                std::cout << "Задание №2\n";
+                task2();
+                std::cout << "-----\n";
+                break;
+            case 3:
+                std::cout << "Задание №3\n";
+                task3();
+                std::cout << "-----\n";
+                break;
+            case 4:
+                std::cout << "Задание №4\n";
+                task4();
+                std::cout << "-----\n";
+                break;
+            default:
+                std::cout << "Номер задачи введен не верно либо не удалось "
+                             "конвертировать введенные данные\n";
+                break;
+            }
+        }
+        catch (const std::exception& e) {
+            std::cerr << "\nERROR: " << e.what() << '\n';
         }
     }
 }
@@ -90,4 +96,11 @@ void task3() {
     std::cout << "Greatest number is " << greatest << "\n";
     std::cout << "Result: MAX-MIN = " << result << "\n";
 }
-void task4() {}
+void task4() {
+    LongNumber A, B;
+    std::cout << "Enter number A: ";
+    std::cin >> A;
+    std::cout << "Enter number B: ";
+    std::cin >> B;
+    std::cout << "Result: A/B = " << A / B << "\n";
+}
