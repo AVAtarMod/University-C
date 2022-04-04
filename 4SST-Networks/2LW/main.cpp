2e97dc2fdc51accf74cbb4e1eeae4f291241ce7a
AVAtar Mod <respawner289@gmail.com>
2022-04-04 18:33:37 +0300

    add 2LW-net

---
+++ 4SST-Networks/2LW/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+void task1();
+
+int main(int argc, char const* argv[])
+{
+    bool noData = false;
+    if (argc == 1) {
+        std::cout << "Введите программу для запуска: ";
+        argc++;
+        noData = true;
+    }
+    for (int i = 1; i < argc; i++) {
+        int choice;
+        if (noData) {
+            std::cin >> choice;
+            std::cin.ignore(32767, '\n');
+        } else {
+            choice = std::stoi(argv[i]);
+        }
+
+        switch (choice) {
+        case 1:
+            std::cout << "Задание №1\n";
+            task1();
+            std::cout << "-----\n";
+            break;
+        default:
+            std::cout << "Номер задачи введён не верно либо не удалось конвертировать введённые данные\n";
+            break;
+        }
+    }
+}
+
+void task1()
+{
+}
