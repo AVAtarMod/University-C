71165ac0b02e14645c7a82d0242fbfbbf49ceb65
AVAtar Mod <respawner289@gmail.com>
2022-06-16 19:56:00 +0300

    [5LW] Update menu. Add features Add SIGINT handler, debug options scanning from commandline paramaters

--- 4SST-Networks/5LW/main.cpp
+++ 4SST-Networks/5LW/main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
+#include <signal.h>
 
 #include "ClientServer.hpp"
 
-int task1();
-int task2();
+sig_atomic_t stopFlag = 0;
+
+int task1(bool debug);
+int task2(bool debug);
+
+void sigintHandler(int)
+{
+    stopFlag = 1;
+}
 
 int main(int argc, char const* argv[])
 {
     bool noData = false;
     if (argc == 1) {
-        std::cout << "Введите программу для запуска: ";
+        std::cout << "Введите программу для запуска и ее параметры: ";
         argc++;
         noData = true;
     }
 
     int exit_code = 0;
 
+    signal(SIGINT, sigintHandler);
+
+    std::cout << "Send SIGINT signal inside task to stop it\n";
+
     for (int i = 1; i < argc; i++) {
+        bool debug = false;
         int choice;
         if (noData) {
             std::cin >> choice;
@@ -25,21 +38,27 @@ int main(int argc, char const* argv[])
             choice = std::stoi(argv[i]);
         }
 
+        if ((choice == 1 || choice == 2) && i < argc - 1 && strcmp(argv[i + 1], "-d") == 0) {
+            debug = true;
+            ++i;
+        }
         switch (choice) {
         case 1:
             std::cout << "Вариант №1\n";
-            exit_code += task1();
+            exit_code += task1(debug);
             std::cout << "-----\n";
             break;
         case 2:
             std::cout << "Вариант №2\n";
-            exit_code += task2();
+            exit_code += task2(debug);
             std::cout << "-----\n";
             break;
         default:
             std::cout << "Номер задачи введён не верно либо не удалось конвертировать введённые данные\n";
             break;
         }
+        if (stopFlag == 1)
+            stopFlag = 0;
     }
 
     return exit_code;
@@ -82,16 +101,25 @@ int charCount(std::string source, char c)
     return count;
 }
 
-int task1()
+void clientServerLoop(ClientServer cs)
+{
+    cs.Start();
+    int bufferLinesCount = -1;
+    while (stopFlag == 0) {
+        cleanConsole(bufferLinesCount);
+        std::string buffer = vectorToText(cs.GetClients());
+        bufferLinesCount = charCount(buffer, '\n');
+    }
+    std::cout << "Closing sockets...\n";
+    cs.Stop();
+}
+
+int task1(bool debug)
 {
     int port;
     std::cout << "Enter port: ";
     std::cin >> port;
-    bool debug = false;
-    cleanConsole(1);
-    std::cout << "Enable debug (1 or 0): ";
-    std::cin >> debug;
-    
+
     ServerOptions serverOpts = ServerOptions();
     serverOpts.method = ServerOptions::byPacket;
     serverOpts.debugOutput = debug;
@@ -102,50 +130,29 @@ int task1()
     clientOpts.debugOutput = debug;
 
     ClientServer cs = ClientServer(serverOpts, clientOpts, port);
-    cs.Start();
-
-    char key;
-    bool exit = false;
-    int bufferLinesCount = -1;
-    while (!exit) {
-        cleanConsole(bufferLinesCount);
-        std::string buffer = vectorToText(cs.GetClients());
-        bufferLinesCount = charCount(buffer, '\n');
-
-        std::cout << buffer << "Enter e for exit: ";
-        std::cin >> key;
-        if (key == 'e')
-            exit = true;
-    }
-    cs.Stop();
+    clientServerLoop(cs);
 
     return 0;
 }
-int task2()
+int task2(bool debug)
 {
+    int port;
+    std::cout << "Enter port: ";
+    std::cin >> port;
+
     ServerOptions serverOpts = ServerOptions();
     serverOpts.method = ServerOptions::byTimeout;
     serverOpts.timeout = std::chrono::milliseconds(1000);
+    serverOpts.debugOutput = debug;
+
     ClientOptions clientOpts = ClientOptions();
     clientOpts.timeout = std::chrono::milliseconds(200);
     clientOpts.sendByTimeout = true;
     clientOpts.useBroadcast = true;
+    clientOpts.debugOutput = debug;
 
-    ClientServer cs = ClientServer(serverOpts, clientOpts, 7000);
-    cs.Start();
-    char key;
-    bool exit = false;
-    int bufferLinesCount = -1;
-    while (!exit) {
-        cleanConsole(bufferLinesCount);
-        std::string buffer = vectorToText(cs.GetClients());
-        bufferLinesCount = charCount(buffer, '\n');
+    ClientServer cs = ClientServer(serverOpts, clientOpts, port);
+    clientServerLoop(cs);
 
-        std::cout << buffer << "Enter e for exit: ";
-        std::cin >> key;
-        if (key == 'e')
-            exit = true;
-    }
-    cs.Stop();
     return 0;
 }
